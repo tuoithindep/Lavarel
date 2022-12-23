26e937da1b9da70ab052f171cb75f7f500f23b23
tuoithindep <nguyenvoan123@gmail.com>
2022-12-23 22:46:31 +0700

    Initial commit

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,10 @@
+<!-- jQuery -->
+<script src="/templates/admin/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="/templates/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="/templates/admin/dist/js/adminlte.min.js"></script>
+
+<script src="/templates/admin/js/main.js"></script>
+
+@yield('footer')
